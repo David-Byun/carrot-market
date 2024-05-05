43932ce0b79a1edd94a0895300e6d13fd4f6c2d6
David Byun <byundavid@naver.com>
2024-05-05 16:20:10 +0900

    cache 처리

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,8 +1,20 @@
 import db from '@/lib/db';
+import getSession from '@/lib/session';
 import { formatToTimeAgo } from '@/lib/utils';
-import { EyeIcon, HandThumbUpIcon } from '@heroicons/react/24/outline';
+import { HandThumbUpIcon as OutlineHandThumbUpIcon } from '@heroicons/react/24/outline';
+import { EyeIcon, HandThumbUpIcon } from '@heroicons/react/24/solid';
+import {
+  revalidatePath,
+  unstable_cache as nextCache,
+  revalidateTag,
+} from 'next/cache';
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
+
+const getCachedPost = nextCache(getPost, ['post-detail'], {
+  tags: ['post-detail'],
+  revalidate: 60,
+});
 
 async function getPost(id: number) {
   /*
@@ -31,15 +43,41 @@ async function getPost(id: number) {
         _count: {
           select: {
             comments: true,
-            likes: true,
           },
         },
       },
     });
     return post;
-  } catch (e) {
-    return null;
-  }
+  } catch (e) {}
+}
+
+function getCachedLikeStatus(postId: number) {
+  const cachedOperation = nextCache(getLikeStatus, ['product-like-status'], {
+    tags: [`like-status-${postId}`],
+  });
+  return cachedOperation(postId);
+}
+
+async function getLikeStatus(postId: number) {
+  const session = await getSession();
+  const isLiked = await db.like.findUnique({
+    where: {
+      id: {
+        postId: postId,
+        userId: session.id!,
+      },
+    },
+  });
+
+  const likeCount = await db.like.count({
+    where: {
+      postId,
+    },
+  });
+  return {
+    likeCount,
+    isLiked: Boolean(isLiked),
+  };
 }
 
 export default async function PostDetail({
@@ -51,18 +89,51 @@ export default async function PostDetail({
   if (isNaN(id)) {
     return notFound();
   }
-  const post = await getPost(id);
+  const post = await getCachedPost(id);
   if (!post) {
     return notFound();
   }
-  console.log(post);
+  const likePost = async () => {
+    'use server';
+    const session = await getSession();
+    try {
+      await db.like.create({
+        data: {
+          postId: id,
+          //TypeScript는 session에 id가 없을수도 있다고 생각하므로, 로그인하지 않은 유저는 아예 이 페이지에 들어올 수 없으므로 느낌표로 있다고 알려줌
+          userId: session.id!,
+        },
+      });
+      revalidateTag(`like-status-${id}`);
+    } catch (e) {}
+  };
+  const dislikePost = async () => {
+    'use server';
+    try {
+      const session = await getSession();
+      console.log(session.id);
+      await db.like.delete({
+        where: {
+          id: {
+            postId: id,
+            //TypeScript는 session에 id가 없을수도 있다고 생각하므로, 로그인하지 않은 유저는 아예 이 페이지에 들어올 수 없으므로 느낌표로 있다고 알려줌
+            userId: session.id!,
+          },
+        },
+      });
+      revalidateTag(`like-status-${id}`);
+    } catch (e) {}
+  };
+
+  const { likeCount, isLiked } = await getCachedLikeStatus(id);
   return (
     <div className="p-5 text-white">
-      <div>
+      <div className="flex items-center gap-2 mb-2">
         <Image
           width={28}
           height={28}
           className="size-7 rounded-full"
+          src={post.user.avatar!}
           alt={post.user.username}
         />
         <div>
@@ -74,15 +145,29 @@ export default async function PostDetail({
       </div>
       <h2 className="text-lg font-semibold">{post.title}</h2>
       <p className="mb-5">{post.description}</p>
-      <div className="flex flex-col gal-5 items-start">
+      <div className="flex flex-col gap-6 items-start">
         <div className="flex items-center gap-2 text-neutral-400 text-sm">
           <EyeIcon className="size-5" />
           <span>조회 {post.views}</span>
         </div>
-        <form>
-          <button>
-            <HandThumbUpIcon className="size-5" />
-            <span>공감하기 ({post._count.likes})</span>
+        <form action={isLiked ? dislikePost : likePost}>
+          <button
+            className={`flex items-center gap-2 text-neutral-400 text-sm border border-neutral-400 rounded-full p-2  transition-colors ${
+              isLiked
+                ? 'bg-orange-500 text-white border-orange-500'
+                : 'hover:bg-neutral-800'
+            }`}
+          >
+            {isLiked ? (
+              <HandThumbUpIcon className="size-5" />
+            ) : (
+              <OutlineHandThumbUpIcon className="size-5" />
+            )}
+            {isLiked ? (
+              <span>{likeCount}</span>
+            ) : (
+              <span>공감하기 ({likeCount})</span>
+            )}
           </button>
         </form>
       </div>
